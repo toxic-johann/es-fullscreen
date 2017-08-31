170ef822379006fe37df21db152daca3cb3dd7a4
toxic-johann <353904974@qq.com>
2017-08-31 21:36:07 +0800

    [update] set width and height to be 100% so that we can really fullscreen what: why: how:

--- src/const.js
+++ src/const.js
@@ -13,7 +13,9 @@ export const DESKTOP_FULLSCREEN_STYLE = {
   top: 0,
   right: 0,
   bottom: 0,
-  overflow: 'hidden'
+  overflow: 'hidden',
+  width: '100%',
+  height: '100%'
 };
 
 export const FULLSCREEN_CHANGE = [
